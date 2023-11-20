e0389adb6750b07d00a6db578d5d7c842917e88a
Alp Berkman <ge73woy@mytum.de>
2023-11-20 14:09:23 +0100

    First commit. Added a Makefile

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define streq(X, Y) (strcmp((X), (Y)) == 0)
+#define pint(X) printf("%i\n", X)
+
+
+int main(/*int argc, char *argv[]*/) {
+
+    return 0;
+}
